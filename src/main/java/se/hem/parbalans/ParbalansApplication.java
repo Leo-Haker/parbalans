777d09179a8b194ce3fbdf53956a050ce1b559d2
Leo Haker <haker338@gmail.com>
2026-07-08 23:46:01 +0200

    Projektstart - Spring Boot skeleton från start.spring.io

---
+++ src/main/java/se/hem/parbalans/ParbalansApplication.java
@@ -0,0 +1,13 @@
+package se.hem.parbalans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParbalansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParbalansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/hem/parbalans/ParbalansApplicationTests.java
@@ -0,0 +1,13 @@
+package se.hem.parbalans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParbalansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
