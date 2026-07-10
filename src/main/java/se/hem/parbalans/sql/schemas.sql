39fe6e226c43ee321fe2d302a427d779ab8ee491
Leo Haker <haker338@gmail.com>
2026-07-10 13:40:41 +0200

    sql databaser

---
+++ src/main/java/se/hem/parbalans/sql/schemas.sql
@@ -0,0 +1,22 @@
+CREATE TABLE person (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE account (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    person1_id BIGINT NOT NULL REFERENCES person(id),
+    person2_id BIGINT NOT NULL REFERENCES person(id)
+);
+
+CREATE TABLE payment (
+    id BIGSERIAL PRIMARY KEY,
+    account_id BIGINT NOT NULL REFERENCES account(id),
+    paid_by_id BIGINT NOT NULL REFERENCES person(id),
+    description VARCHAR(255) NOT NULL,
+    amount NUMERIC(12,2) NOT NULL,
+    own_share_percentage INT NOT NULL DEFAULT 50,
+    date DATE NOT NULL
+);
